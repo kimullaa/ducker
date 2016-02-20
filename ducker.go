e7ed625013d404ce886bda13e0515cd99923ef3d
Kimura <hogehoge@gmail.com>
2016-02-21 00:52:18 +0900

    create

--- ducker.go
+++ ducker.go
@@ -5,27 +5,63 @@ import(
     "strconv"
     "strings"
     "os"
+    "flag"
     "io"
     "os/exec"
     "time"
     "log"
 )
 
+const (
+    RUN = "run"
+    PS = "ps"
+    PULL = "pull"
+)
+
 var out io.Writer = os.Stdout
 
-var duck_ascii_target = []string{
-`
-              o 
-  ___       oo  
- /    \\   o    
-<  o   \\       
- \\    |___     
- /    _____\\_  
- |    ''''  \\  
- \\__________|  
- 
-`}
+// MAP[COMMAND , AsciiArt]
+var m  = map[string] []string {
+RUN : strings.Split(
+`        
+          
+  ___   
+ / "" \\  I'm a highway star
+<  o   \     
+ \     |___     
+ /    _____\<      3
+ |    ''''" \  ＝＝ 3
+ \\_________/      3
+     \ \ 
+     ` , "\n"),
+PULL : strings.Split(
+`        
+             
+         ___    someone is pulling me
+        / "" \
+<<<<<<<<  o   \     
+        \     |___     
+        /    _____\<   
+        |    ''''" ~\  
+        \\~~~~~~~~~~/ 
+        
+     ` , "\n"),
+PS : strings.Split(
+`        
+        
+  ___    
+ / "" \\  p.s. my holiday has gone to waste
+<  o   \   
+ \     |___     
+ /    _____\<
+ |    ''''" \ 
+ \\_________/
+ ～～～～～～～～～～～
+     ` , "\n"),
+}
 
+
+// return current terminal window (width,height)
 func getTerminalSize() (width,height int) {
     cmd := exec.Command("stty","size")
     cmd.Stdin = os.Stdin
@@ -33,36 +69,72 @@ func getTerminalSize() (width,height int) {
 
     if err != nil {
 	log.Fatal(err)
-	//default 
+	//default size
         width  = 70
         height = 10
 	return 
-    }else {
-    tmp := strings.Split(string(out)," ");
-    fmt.Println(tmp)
-    width,err  = strconv.Atoi(tmp[0])
-    height,err = strconv.Atoi(tmp[1])
-    return 
-
+    } else {
+	// command's result pattern => [width height\n]
+        tmp := strings.Split(strings.Trim(string(out),"\n")," ");
+        height,err  = strconv.Atoi(tmp[0])
+        width,err = strconv.Atoi(tmp[1])
+        return 
     }
 }
 
 
+func decideCursolPosition() (width,height int) {
+    width,height = getTerminalSize()
+    height = 5
+    return 
+}
+
+
+type Ascii struct {
+    Texts []string
+    StopTimeInMillis time.Duration
+}
+
+func NewAscii(command string) *Ascii {
+
+    ascii := &Ascii{}
+
+    switch  command{
+    case RUN: 
+        ascii.Texts = m[RUN]
+        ascii.StopTimeInMillis = 10 * time.Millisecond
+    case PULL:
+        ascii.Texts = m[PULL]
+        ascii.StopTimeInMillis = 20 * time.Millisecond
+    case PS:
+        ascii.Texts = m[PS]
+        ascii.StopTimeInMillis = 20 * time.Millisecond
+    default:
+        ascii.Texts = m[PS]
+        ascii.StopTimeInMillis = 50 * time.Millisecond
+    }
+
+    return ascii
+}
+
+
+
 func main(){
-    //画面をリフレッシュ
+
+    flag.Parse()
+    ascii := NewAscii(flag.Arg(0))
+
+    // clear a window
     fmt.Fprintln(out,"\x1b[2J")
 
-    w_width,w_height := getTerminalSize()
-    fmt.Println(w_width,w_height)
-
-    duck_ascii := strings.Split(duck_ascii_target[0], "\n")
-    for width := w_width; width > 0; width-- {
-	for height := 5; height < 5 + len(duck_ascii); height++{
-	    //カーソル位置ズラしながら描画
-            fmt.Fprintf(out,"\x1b[%d;%dH\x1b[K",height,width)
-	    fmt.Print(duck_ascii[height-5])
+    // print AA
+    start_width,start_height := decideCursolPosition()
+    for width := start_width; width > 0; width-- {
+	for i := 0; i < len(ascii.Texts); i++{
+            fmt.Fprintf(out,"\x1b[%d;%dH\x1b[2K",start_height+i,width)
+	    fmt.Print(ascii.Texts[i])
 	}
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(ascii.StopTimeInMillis)
     }
 
 }
